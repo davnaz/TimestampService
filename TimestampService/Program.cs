b903b6fe21542079a01b1add4b86193c2949845f
Назлуян Давид <davnazzz@gmail.com>
2018-10-26 22:28:04 +0300

    Добавьте файлы проекта.

---
+++ TimestampService/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimestampService
+{
+    class Program
+    {
+        private static Random random = new Random();
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            int currsec = dateTime.Second;
+            string currTableName = String.Format("data_{0}_{1}_{2}_{3}_{4}_{5}",dateTime.Year, dateTime.Month.ToString().PadLeft(2, '0'), dateTime.Day.ToString().PadLeft(2, '0'), dateTime.Hour.ToString().PadLeft(2, '0'), dateTime.Minute.ToString().PadLeft(2, '0'), dateTime.Second.ToString().PadLeft(2,'0'));
+            SqlCommand sqlCommand = new SqlCommand(String.Format("CREATE TABLE[dbo].[{0}]([timestamp][datetime2](7) NULL,[text] [char](10) NULL) ON[PRIMARY]",currTableName), new SqlConnection("Data Source=DAVIDOMEN;Initial Catalog=homework;Integrated Security=True"));
+            sqlCommand.Connection.Open();
+            for(int i = 0;i<10;i++)
+            {
+                sqlCommand.ExecuteNonQuery();
+                while (currsec == DateTime.Now.Second)
+                {
+                    //генерируем инсёрты
+                    //Console.WriteLine(currsec);
+                    sqlCommand.CommandText = String.Format("INSERT INTO [dbo].[{2}]  ([timestamp],[text]) VALUES('{0}','{1}')", dateTime.ToString("yyyy-MM-dd HH:mm:ss.fff"), RandomString(7),currTableName);
+                    sqlCommand.ExecuteNonQuery();
+                }
+                
+                dateTime = DateTime.Now;
+                currsec = dateTime.Second;
+                currTableName = String.Format("data_{0}_{1}_{2}_{3}_{4}_{5}",dateTime.Year, dateTime.Month.ToString().PadLeft(2, '0'), dateTime.Day.ToString().PadLeft(2, '0'), dateTime.Hour.ToString().PadLeft(2, '0'), dateTime.Minute.ToString().PadLeft(2, '0'), dateTime.Second.ToString().PadLeft(2, '0'));
+                sqlCommand.CommandText = String.Format("CREATE TABLE[dbo].[{0}]([timestamp][datetime2](7) NULL,[text] [char](10) NULL) ON[PRIMARY]", currTableName);
+            }
+            
+
+
+
+
+        sqlCommand.Connection.Close();
+
+        }
+        public static string RandomString(int length)
+        {
+             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+             return new string(Enumerable.Repeat(chars, length)
+               .Select(s => s[random.Next(s.Length)]).ToArray());
+            //return "AAAAA";
+        }
+    }
+}
---
+++ TimestampService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TimestampService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TimestampService")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ed46760e-6b99-4bb9-a0a0-cec19a7a8fcc")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
